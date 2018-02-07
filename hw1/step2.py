9ba0f506b8aad00603633eb8e65e6cfd29cf3caa
Basil Vetas <basilvetas@gmail.com>
2018-02-07 08:09:29 -0500

    protobuffer reply tweets

--- hw1/step2.py
+++ hw1/step2.py
@@ -1,23 +1,25 @@
-from json import loads
-from itertools import imap
 import twitter_pb2
 
 def decode():
 
 	tweets = twitter_pb2.Tweets()
 
-	deleted_count = 0
+	deleted_tweet_count = 0
+	replies_tweet_count = 0
 
 	with open('twitter.pb', 'rb') as f:
 		tweets.ParseFromString(f.read())
-		for tweet in tweets.tweets:
+
+		for tweet in tweets.tweets:			
+			
 			if(tweet.is_delete):
-				deleted_count += 1
+				deleted_tweet_count += 1			
+			else:	
+				if(tweet.insert.reply_to):							
+					replies_tweet_count += 1
 
-
-	print deleted_count
-
-
+	print deleted_tweet_count	
+	print replies_tweet_count
 
 if __name__ == "__main__":
 	decode()
