ecaae9869fd74ee37a10c0becb6c4093d6ce0982
Basil Vetas <basilvetas@gmail.com>
2018-02-07 08:45:50 -0500

    completed protobuffer

--- hw1/step2.py
+++ hw1/step2.py
@@ -1,4 +1,5 @@
 import twitter_pb2
+import operator
 
 def decode():
 
@@ -6,20 +7,29 @@ def decode():
 
 	deleted_tweet_count = 0
 	replies_tweet_count = 0
+	uid_tweet_counts = {}
 
 	with open('twitter.pb', 'rb') as f:
 		tweets.ParseFromString(f.read())
 
 		for tweet in tweets.tweets:			
-			
-			if(tweet.is_delete):
+
+			if(tweet.is_delete):				
 				deleted_tweet_count += 1			
 			else:	
 				if(tweet.insert.reply_to):							
-					replies_tweet_count += 1
+					replies_tweet_count += 1				
+				if not tweet.insert.uid in uid_tweet_counts:
+					uid_tweet_counts[tweet.insert.uid] = 1
+				else:
+					uid_tweet_counts[tweet.insert.uid] += 1	
 
-	print deleted_tweet_count	
-	print replies_tweet_count
+	most_tweets = sorted(uid_tweet_counts.items(), key=operator.itemgetter(1), reverse=True)[:5]	
+	print "deleted tweet count:", deleted_tweet_count	
+	print "replies tweet count:", replies_tweet_count
+
+	for uid, count in most_tweets:
+		print "uid:", uid, "- tweet count:", count
 
 if __name__ == "__main__":
 	decode()
