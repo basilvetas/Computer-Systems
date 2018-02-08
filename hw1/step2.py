7480d899146aabc3320d7dd86fcf832f268783bd
Basil Vetas <basilvetas@gmail.com>
2018-02-08 18:21:14 -0500

    analysis for q's

--- hw1/step2.py
+++ hw1/step2.py
@@ -3,9 +3,7 @@ import operator
 
 def decode():
 	tweets = twitter_pb2.Tweets()
-	deleted_tweet_count = 0
-	replies_tweet_count = 0
-	uid_tweet_counts = {}
+	deleted_tweet_count, replies_tweet_count, uid_tweet_counts = 0, 0, {}
 
 	with open('twitter.pb', 'rb') as f:
 		tweets.ParseFromString(f.read())
@@ -21,8 +19,8 @@ def decode():
 					uid_tweet_counts[tweet.insert.uid] += 1	
 
 	most_tweets = sorted(uid_tweet_counts.items(), key=operator.itemgetter(1), reverse=True)[:5]	
-	print "1. Find the number of deleted messages in the dataset.\n", deleted_tweet_count	
-	print "2. Find the number of tweets that are replies to another tweet.\n", replies_tweet_count
+	print "1. Find the number of deleted messages in the dataset.", deleted_tweet_count	
+	print "2. Find the number of tweets that are replies to another tweet.", replies_tweet_count
 	print "3. Find the five user IDs (field name: uid) that have tweeted the most."
 	for uid, count in most_tweets:
 		print "uid:", uid, "- tweet count:", count
@@ -30,10 +28,8 @@ def decode():
 if __name__ == "__main__":
 	decode()
 
-# 1. Find the number of deleted messages in the dataset.
-# 1554
-# 2. Find the number of tweets that are replies to another tweet.
-# 2531
+# 1. Find the number of deleted messages in the dataset. 1554
+# 2. Find the number of tweets that are replies to another tweet. 2531
 # 3. Find the five user IDs (field name: uid) that have tweeted the most.
 # uid: 1269521828 - tweet count: 5
 # uid: 392695315 - tweet count: 4
